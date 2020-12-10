66441948ffe13dc4fc79e63c0105c635d43c803c
Evan <zx086401@gmail.com>
2020-12-10 23:40:40 +0800

    Define models

---
+++ src/model/DisplayData.tsx
@@ -0,0 +1,6 @@
+export interface DisplayData {
+  x: number,
+  y: number,
+  width: number,
+  height: number
+}
---
+++ src/model/SelectedArea.tsx
@@ -0,0 +1,9 @@
+export interface SelectedArea {
+  id: number
+  x: number
+  y: number
+  relativeX: number
+  relativeY: number
+  width: number
+  height: number
+}
